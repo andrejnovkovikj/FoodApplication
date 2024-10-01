75921c9a84b387ab4100389685648eeceb5d448a
andrejnovkovikj <novkovikjandrej@gmail.com>
2024-10-01 13:17:17 +0200

    cross platform for ingredients

--- src/main/java/com/example/FoodApp/web/api/IngredientApiController.java
+++ src/main/java/com/example/FoodApp/web/api/IngredientApiController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/ingredients")
+@CrossOrigin(origins = {"http://localhost:3000"})
 public class IngredientApiController {
     private final IngredientService ingredientService;
 
